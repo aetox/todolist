4870cd80298cba6bde92393a1148fe82b587cbc8
aetox <core.editor>
2023-05-25 11:27:04 +0200

    Style to do list

--- src/app/todolist-item/todolist-item.component.ts
+++ src/app/todolist-item/todolist-item.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-todolist-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './todolist-item.component.html',
   styleUrls: ['./todolist-item.component.scss']
 })
--- src/app/todolist-search-bar/todolist-search-bar.component.ts
+++ src/app/todolist-search-bar/todolist-search-bar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
 
 @Component({
   selector: 'app-todolist-search-bar',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule,InputTextModule],
   templateUrl: './todolist-search-bar.component.html',
   styleUrls: ['./todolist-search-bar.component.scss']
 })
